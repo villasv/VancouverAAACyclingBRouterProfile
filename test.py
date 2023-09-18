be4d02e4b70203f1e02ff420caa7dfa619220ba1
Victor Villas <mail@victor.villas>
2023-09-17 17:08:52 -0700

    Introduce docs and python package

--- test.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-class Router:
-    pass
-
-
-if __name__ == "__main__":
-    router = Router()
---
+++ vancycling_brouter_profile/test.py
@@ -0,0 +1,22 @@
+from typing import List
+import logging
+import requests
+
+BROUTER_BASE_URL = "https://www.brouter.de/brouter"
+"?lonlats=-123.126527,49.289796|-123.095911,49.28404&profile=custom_1694894568334&alternativeidx=0&format=geojson"
+
+
+def set_profile(profile):
+    pass
+
+
+def get_route(points: List):
+    pass
+
+
+class Router:
+    pass
+
+
+if __name__ == "__main__":
+    router = Router()
